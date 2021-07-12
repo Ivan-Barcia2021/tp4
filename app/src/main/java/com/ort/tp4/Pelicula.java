f6315b7d8f430344d2348d14eacbcb6a608b3975
Ivan Barcia <45148013@est.ort.edu.ar>
2021-07-12 00:38:47 -0300

    tp4

---
+++ app/src/main/java/com/ort/tp4/Pelicula.java
@@ -0,0 +1,11 @@
+package com.ort.tp4;
+
+import java.util.ArrayList;
+
+public class Pelicula {
+    public ArrayList<String> obtenertodas(){
+        ArrayList<String> listadevolver= new ArrayList<> ();
+//desarrollar la clase y mostrar en listviews
+        return listadevolver;
+    }
+}
---
+++ app/src/main/java/com/ort/tp4/resultados.java
@@ -0,0 +1,15 @@
+package com.ort.tp4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados);
+
+    }
+}
